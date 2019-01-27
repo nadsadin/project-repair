311e669e7daf18ba28fcf2cd9b936008962e7f1d
Nadsadin <leloushlampergh@gmail.com>
2019-01-27 15:41:12 +0300

    finish order page

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,9 +1,17 @@
 
 $(function() {
-  $('#smartwizard').smartWizard({
-    autoAdjustHeight: false,
-    backButtonSupport: false,
-    useURLhash: false,
-    showStepURLhash: false
-  });
+  // $('#smartwizard').smartWizard({
+  //   autoAdjustHeight: false,
+  //   backButtonSupport: false,
+  //   useURLhash: false,
+  //   showStepURLhash: false,
+  //   toolbarSettings: {
+  //     toolbarPosition: 'none'
+  //   },
+  //   anchorSettings: {
+  //     anchorClickable: true,
+  //     enableAllAnchors: false
+  //   },
+  //   disabledSteps: []
+  // });
 });
